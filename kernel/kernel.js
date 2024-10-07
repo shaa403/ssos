dd8f7ea481b4ebc75256e6930218874458c44fbf
shaa403 <da40audu40@gmail.com>
2024-10-07 16:28:22 +0100

    modified the os start message @@@ shaa403

--- kernel/kernel.js
+++ kernel/kernel.js
@@ -4,7 +4,7 @@
 import cors from "./cors.js";
 import { createServer } from "node:http";
 
-const listencb = port => console.log("Running the system on http://localhost:%s", port);	
+const listencb = port => console.log("\nCreated an instance on http://localhost:%s", port);	
 
 const start = port => createServer(cors).listen(port, ()=> listencb(port));
 
