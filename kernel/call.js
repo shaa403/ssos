98fa3a815084269c9eb6bf6d113393dadfafdc87
shaa403 <da40audu40@gmail.com>
2024-10-07 11:36:01 +0100

    0 @@@ shaa403

---
+++ kernel/call.js
@@ -0,0 +1,5 @@
+
+
+export default function handle_call(request, response) {
+	console.log("working hehe");
+}
---
+++ kernel/graphic.js
@@ -0,0 +1,5 @@
+
+
+export default function handle_graphic_call(request, response) {
+	console.log("working");
+}
