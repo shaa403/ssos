5aae82fc68c37ff44418c36955487b1fcd741194
shaa403 <da40audu40@gmail.com>
2024-10-11 12:23:47 +0100

    added "GET_NET_STATUS" sys call -> new

--- api/sys.js
+++ api/sys.js
@@ -1,6 +1,8 @@
 
 "use strict";
 
+import { networkInterfaces } from "node:os";
+
 import batteryLevel from "battery-level";
 import isCharging from "is-charging";
 import res_construct from "../kernel/res.js";
@@ -11,4 +13,21 @@ export async function get_battery_status(request, response) {
   const ischarging= await isCharging();
   response.statusCode = 200;
   response.end(res_construct(true, null, { lvl, ischarging  }));
+}
+
+
+export function get_net_stats(response) {
+  const net_interfaces = Object.keys(networkInterfaces()).map(intfc => intfc.toLowerCase());
+  let isConnectedToAnotherNet;	
+  if ( 
+       net_interfaces.includes("wlo1") || 
+       net_interfaces.includes("eth0") ||
+       net_interfaces.includes("en0") ||
+       net_interfaces.includes("ethernet") ||
+       net_interfaces.includes("wi-fi") ||
+       net_interfaces.includes("wifi")
+     ) isConnectedToAnotherNet = true
+  else isConnectedToAnotherNet = false;
+  response.statusCode = 200;
+  response.end(res_construct(true, null, isConnectedToAnotherNet));
 }
--- kernel/call.js
+++ kernel/call.js
@@ -3,7 +3,7 @@
 
 import res_construct from "./res.js";
 import { lock_setup } from "../boot/interface.js";
-import { get_battery_status } from "../api/sys.js";
+import { get_battery_status, get_net_stats } from "../api/sys.js";
 import { get_users, signin, verify_user } from "../api/usr.js";
 
 /**
@@ -22,6 +22,7 @@ export default function handle_call(request, response) {
     else if (call === "SIGNIN") signin(request, response);
     else if (call === "VERIFYUSER") verify_user(request, response, false);
     else if (call === "GET_BATTERY_STATUS") get_battery_status(request, response);
+    else if (call === "GET_NET_STATUS") get_net_stats(response);
     else {
       response.statusCode = 403;
       response.end(res_construct(false, "UNKNOWN_SYS_CALL"));
