956800001776a8a93970aa2c7c5680ba19f6f76a
shaa403 <da40audu40@gmail.com>
2024-10-11 11:53:16 +0100

    integrated the "GET_BATTERY_STATUS" syc call on main screen -> mod

--- screen/main/scripts/screen_manager.js
+++ screen/main/scripts/screen_manager.js
@@ -1,15 +1,33 @@
 
 
 document.querySelector("#menu-bar-time").innerHTML = getTime();
+getBatteryStats();
 
 function getTime() {
   const date = new Date();
   const hh = date.getHours();
   return `${hh < 10 ? '0'+ hh : hh}:${date.getMinutes()}`;		
 }
+function getBatteryStats() {
+  fetch(window.location.origin, {
+  	 method: "POST",
+  	 headers: {
+  	 	"x-call-type": "ng"
+  	 }, 
+  	 body: JSON.stringify({ call: "GET_BATTERY_STATUS" })
+  }).then(data => {
+     return data.json()
+  }).then(data => {
+     if (data.ack === true) {
+        if(!data.data.ischarging) document.querySelector("#menu-bar-battery-charing").style.display = "none";
+        document.querySelector("#menu-bar-battery-percentage").innerHTML = data.data.lvl * 100 +'%';
+     }
+  })	
+}
  
 setInterval(()=> {
   document.querySelector("#menu-bar-time").innerHTML = getTime();
+  getBatteryStats();
 },1000 * 30);
   /* Not up to 60s "on purpose" to sync since the amount of seconds left till "new time" depends on the 
-  time at which this script started execution */
+  time at which this script started execution. this is also a good interval for fetching battery stats */
